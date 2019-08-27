b1d7b56e294777d75cbe23c0993e3990209cb056
antestor86 <54534919+antestor86@users.noreply.github.com>
2019-08-27 11:33:59 +0400

    Add files via upload

---
+++ Lesson1.py
@@ -0,0 +1,13 @@
+print(3>10)
+print(3==3.0)
+print(True)
+print('4>3=',4>3)
+print('4>5=',4>5)
+
+#Boolean Logic
+print('True and False',True and False)
+print('True or Fals',True or False)
+print(False or False)
+
+#None
+
