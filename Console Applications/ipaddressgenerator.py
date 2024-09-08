856f929a77ceed5d9082f8a6bada8599866fd83d
antestor86 <polygon86.86@gmail.com>
2024-09-09 00:07:21 +0400

    IP Address generator

---
+++ Console Applications/ipaddressgenerator.py
@@ -0,0 +1,9 @@
+number = int(input('Set first number:'))
+step = int(input("Set second number:"))
+summ = 0
+print('\nIP-Address:',end=' ')
+for count in range(3):
+    print(number,end='.')
+    summ +=number
+    number += step
+# print(summ)
