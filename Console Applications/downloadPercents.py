b757f4e67ae33b8f6683d354e436ca0d1528bd32
antestor86 <polygon86.86@gmail.com>
2024-10-30 21:49:53 +0400

    Create mega calculator

---
+++ Console Applications/downloadPercents.py
@@ -0,0 +1,13 @@
+file_size = int(input("Set File size: "))
+connection = int(input("Set connection speed: "))
+downloaded = connection
+seconds = 1
+percents = 0
+for file in range(connection,file_size):
+        percents = round(downloaded / file_size*100)
+        print(seconds, 'sec', '.Downloaded', downloaded, 'from', file_size, 'MB','(',percents,'%)')
+        downloaded += connection
+        seconds += 1
+        if downloaded >= file_size:
+            print(seconds, 'sec', '.Downloaded', file_size, 'from', file_size, 'MB','(',100,'%)')
+            break
